b78f8ce2d2e9da161d080b8a2f06fcb4915d6d23
Bogdan <ibm86@cloud.linge-ma.ws>
2017-02-24 18:25:25 +0200

    initial commit

---
+++ plugin.py
@@ -0,0 +1,165 @@
+###
+# Copyright (c) 2016, Znuff
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+###
+
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+import re
+from urllib2 import urlopen, URLError, quote
+import requests
+from bs4 import BeautifulSoup
+
+try:
+    from supybot.i18n import PluginInternationalization
+    _ = PluginInternationalization('Bluray')
+except ImportError:
+    # Placeholder that allows to run the plugin on a bot
+    # without the i18n module
+    _ = lambda x: x
+
+def fetch(movie=False):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2729.3 Safari/537.36'}
+
+    if movie:
+        #data = {'section':'bluraymovies','userid':'-1','country':'US','keyword': quote(movie)}
+        data = 'section=bluraymovies&userid=-1&country=US&keyword=' + quote(movie)
+        url = 'http://www.blu-ray.com/search/quicksearch.php'
+
+    try:
+        resp = utils.web.getUrl(url, headers=headers, data=data)
+    except utils.web.Error, e:
+        resp = False
+    return resp
+
+def get(url=False, post=False, data=False):
+    headers = {
+            'Pragma': 'no-cache',
+            'Accept-Language': 'en-US,en;q=0.8,ro;q=0.6',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.13 Safari/537.36',
+            'Accept': '*/*',
+            'Referer': 'http://www.dvdsreleasedates.com/',
+            'Cache-Control': 'no-cache',
+            }
+    if url:
+        url = 'http://www.dvdsreleasedates.com' + url
+
+    try:
+        if post:
+            resp = requests.post(url, headers=headers, data=data)
+        else:
+            resp = requests.get(url, headers=headers)
+    except:
+        resp = False
+
+    return resp
+
+class Bluray(callbacks.Plugin):
+    """Blu-Ray release dates"""
+    threaded = True
+
+    def bd(self, irc, msg, args, movie):
+        """-- usage: bd <movie>
+
+        """
+
+        movie = fetch(movie)
+    
+        if movie:
+            p = re.compile(ur'\#ccc">(.*)</(?:.*)&nbsp;(.*)')
+            data = re.search(p, movie)
+            irc.reply(format('%s: %s',
+                ircutils.bold(data.group(2)),
+                data.group(1)))
+        else:
+            irc.reply('The chicken has stolen the coconut :-(')
+
+    bd = wrap(bd, ['text'])
+
+    def br(self, irc, msg, args, movie):
+        """-- usage: br <movie>
+
+        """
+
+        # livesearch first
+        url = '/livesearch.php?q=' + movie
+
+        response = get(url)
+
+        if response:
+            soup = BeautifulSoup(response.content)
+            url = soup.find('a')
+            
+            # follow the first link, if it exists
+            if url:
+                url = url['href']
+                response = get(url)
+
+            # if not, let's try the non-live search form
+            else:
+                url = '/search.php'
+                data = {'searchStr' : movie}
+                response = get(url, True, data)
+                # results page
+                if response:
+                    soup = BeautifulSoup(response.content)
+                    url = soup.find('td', {'class' :
+                        'dvdcell'}).find('a')['href']
+                    # find the first link
+                    response = get(url)
+
+        if response:
+            soup = BeautifulSoup(response.content)
+            dates = soup.find('h2')
+            moviename = soup.find('h1').find('span').text
+            if dates:
+                dates = dates.findAll('span')
+                if len(dates) > 1:
+                    irc.reply(format('%s: %s (Blu-ray), %s (digital)',
+                            ircutils.bold(moviename.strip()),
+                            ircutils.mircColor(dates[0].text, 'green'),
+                            ircutils.mircColor(dates[1].text, 'red')))
+                else:
+                    irc.reply(format('%s: %s',
+                            ircutils.bold(moviename.strip()),
+                            ircutils.mircColor(dates[0].text, 'green')))
+            else:
+                irc.reply(format('%s: shit\'s too old',
+                    ircutils.bold(moviename.strip())))
+
+        else:
+            irc.reply('The coconut has resisted our attempts!')
+        
+    br = wrap(br, ['text'])
+
+Class = Bluray
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
